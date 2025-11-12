b4fa6c7081648aeb872703585c0a5ab39e360281
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-11-12 10:43:58 +0100

    red  test rate note 10 is acceptable

--- test/test_rate_note.py
+++ test/test_rate_note.py
@@ -4,3 +4,5 @@ from src.rate_note import rate_note
 
 def test_rate_note_9_is_unsuccessful():
     assert rate_note(9)=="unsuccessful"
+def test_rate_note_10_is_acceptable():
+    assert rate_note(10)=="acceptable"
