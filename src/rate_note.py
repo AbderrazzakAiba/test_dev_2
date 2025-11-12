578c372adaf74baba428b401828dd679cc59f7d2
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-11-12 16:15:21 +0100

    blue-refactoring

--- src/rate_note.py
+++ src/rate_note.py
@@ -1,6 +1,8 @@
 def rate_note(note :int)->str:
     if note<10 :
      return "unsuccessful"
-    if note==12:
-        return "good"
-    return "acceptable"
+    if note==10:
+        return "acceptable"
+    if note== 12 :
+     return "good"
+
