15d961f87aeafb80a22a2fb53b95ccfe22f3a64e
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-11-12 15:59:45 +0100

    red-rate 12 return good

--- test/test_rate_note.py
+++ test/test_rate_note.py
@@ -9,3 +9,5 @@ def test_rate_note_is_unsuccessful(note):
 
 def test_rate_note_10_is_acceptable():
     assert rate_note(10)=="acceptable"
+def test_rate_note_12_is_good():
+    assert rate_note(12)=="good"
